7618e8b467ca9350fd0e4a8e7d9ee98f506757a9
Cole Hudson <cole@colejhudson.com>
2019-03-22 08:37:02 -0700

    added part 2

--- sprint-challenge/acme.py
+++ sprint-challenge/acme.py
@@ -7,3 +7,23 @@ class Product:
         self.weight = weight
         self.flammability = flammability
         self.identifier = random.randint(1000000, 9999999)
+
+    def stealability(self):
+        price_per_unit = self.price/self.weight
+
+        if price_per_unit < 0.5:
+            return "Not so stealable..."
+        elif 0.5 <= price_per_unit < 1:
+            return "Kinda stealable."
+        else:
+            return "Very stealable!"
+
+    def explode(self):
+        explodability = self.flammability * self.weight
+
+        if explodability < 10:
+            return "...fizzle."
+        elif 10 <= explodability < 50:
+            return "...boom!"
+        else:
+            return "...BABOOM!"
