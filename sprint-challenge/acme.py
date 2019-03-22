fbc7c78e5008ff8079455dfbaafedb361b721680
Cole Hudson <cole@colejhudson.com>
2019-03-22 08:36:22 -0700

    moved sprint challenge description

---
+++ sprint-challenge/acme.py
@@ -0,0 +1,9 @@
+import random
+
+class Product:
+    def __init__(self, name, price=10, weight=20, flammability=0.5):
+        self.name = name
+        self.price = price
+        self.weight = weight
+        self.flammability = flammability
+        self.identifier = random.randint(1000000, 9999999)
